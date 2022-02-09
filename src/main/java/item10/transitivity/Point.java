745d94007c2e8452c658bc93be57347c5b978949
eunhee <dmsgml9903@gmail.com>
2022-02-09 14:40:15 +0900

    [ADD] item10

---
+++ src/main/java/item10/transitivity/Point.java
@@ -0,0 +1,28 @@
+package item10.transitivity;
+
+import java.util.Objects;
+
+public class Point {
+    // 구체 클래스를 확장해 새로운 값을 추가하면서 equals규약을 만족시킬 방법은 존재하지 않는다.
+    // 우회 방법 : 상속 대신 컴포지션 사용
+    private final int x;
+    private final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x && y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
