89079008a8d232ae58e4dc06400cd103bf289497
eunhee <dmsgml9903@gmail.com>
2022-03-02 19:33:29 +0900

    item13

---
+++ src/main/java/item13/case2/B.java
@@ -0,0 +1,4 @@
+package item13.case2;
+
+public class B {
+}
---
+++ src/main/java/item13/case3/A.java
@@ -0,0 +1,18 @@
+package item13.case3;
+
+public class A {
+
+    private int number; // 기본 타입
+    private String name; // 불변 객체
+
+    public A(int number, String name) {
+        this.number = number;
+        this.name = name;
+    }
+
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
