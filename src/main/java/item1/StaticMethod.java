6b2e8364a94190e84480d5652f314d2ecf60b98f
eunhee <dmsgml9903@gmail.com>
2022-03-20 19:16:39 +0900

    [item1]

---
+++ src/main/java/item1/StaticMethod.java
@@ -0,0 +1,14 @@
+package item1;
+
+public class StaticMethod {// 불변 클래스
+
+    private static final StaticMethod INSTANCE = new StaticMethod();
+
+    private StaticMethod() {
+
+    }
+
+    public static StaticMethod getInstance() {
+        return INSTANCE;
+    }
+}
