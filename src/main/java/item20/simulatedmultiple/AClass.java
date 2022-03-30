003dc7d0790ef3b67845eb96104bfe6a669c6b75
eunhee <dmsgml9903@gmail.com>
2022-03-30 10:43:36 +0900

    [ADD] item21, item22 요약정리

--- src/main/java/item20/simulatedmultiple/AClass.java
+++ src/main/java/item20/simulatedmultiple/AClass.java
@@ -4,7 +4,7 @@ import java.util.AbstractMap;
 
 // 시뮬레이트한 다중 상속 : 다중 상속의 많은 장점을 제공하는 동시에 단점은 피하게 된다.
 public class AClass implements AInterface{// , BInterface.., CInterface
-    
+
     private InnerAbstractAInterface inner = new InnerAbstractAInterface();
 
     @Override
