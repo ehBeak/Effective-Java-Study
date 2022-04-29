74c4c7ee3655ae22dd7c06550fdec1882ec53709
eunhee <dmsgml9903@gmail.com>
2022-04-29 21:38:14 +0900

    [ADD] item31

---
+++ src/main/java/item31/Stack/Stack.java
@@ -0,0 +1,54 @@
+package item31.Stack;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.EmptyStackException;
+
+public class Stack<E> {
+
+    private Object[] elements;
+    private int size = 0;
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+
+    public Stack() {
+        elements = new Object[DEFAULT_INITIAL_CAPACITY];
+    }
+
+   public void push(E e) {
+        ensureCapacity();
+        elements[size++] = e;
+    }
+    public E pop() {
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+        @SuppressWarnings("unchecked") E result = (E) elements[--size];
+
+        elements[size] = null;
+        return result;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private void ensureCapacity() {
+        if (elements.length == size) {
+            elements = Arrays.copyOf(elements, 2 * size + 1);
+        }
+    }
+
+    // 일련의 원소를 스택에 넣는 메서드
+    public void pushAll(Iterable<E> src) {
+        for (E e : src) {
+            push(e);
+        }
+    }
+
+    // Stack 안의 모든 원소를 주어진 컬렉션으로 옮겨 담는다.
+    public void popAll(Collection<E> dst) {
+        while (!isEmpty()) {
+            dst.add(pop());
+        }
+    }
+}
---
+++ src/main/java/item31/max/Max.java
@@ -0,0 +1,43 @@
+package item31.max;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+
+public class Max {
+
+    // 컬렉션에서 최댓값을 반환한다. - 재귀적 타입 한정 사용, 한정적 와일드 카드
+    // ? extends E: ScheduledFuture<?>를 상속받은(확장한) 것
+    // ScheduleFuture<V> 는 Comparable<E>를 상속받았기 때문에, 즉 상위 타입이기 때문에 오류 없음
+    public static <E extends Comparable<? super E>> E max(List<? extends E> list) {
+        if (list.isEmpty()) {
+            throw new IllegalArgumentException("컬렉션이 비어 있습니다.");
+        }
+
+        E result = null;
+        for (E e : list) {
+            if (result == null || e.compareTo(result) > 0) {
+                result = Objects.requireNonNull(e);
+            }
+        }
+
+        return result;
+    }
+
+    // 컬렉션에서 최댓값을 반환한다. - 재귀적 타입 한정 사용, 한정적 와일드 카드 사용안함
+    //E: ScheduledFuture<?> -> Comparable<ScheduleFuture<?>>를 구현해야함
+    public static <E extends Comparable<E>> E max_x(List<E> list) {
+        if (list.isEmpty()) {
+            throw new IllegalArgumentException("컬렉션이 비어 있습니다.");
+        }
+
+        E result = null;
+        for (E e : list) {
+            if (result == null || e.compareTo(result) > 0) {
+                result = Objects.requireNonNull(e);
+            }
+        }
+
+        return result;
+    }
+}
