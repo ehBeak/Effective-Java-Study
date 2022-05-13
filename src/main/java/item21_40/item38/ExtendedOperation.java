c0a0aafe795a4dae5040863300aeec5873b3ebb0
eunhee <dmsgml9903@gmail.com>
2022-05-13 15:54:24 +0900

    [ADD] item38

---
+++ src/main/java/item21_40/item38/ExtendedOperation.java
@@ -0,0 +1,55 @@
+package item21_40.item38;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+public enum ExtendedOperation implements Operation {
+    EXP("^") {
+        @Override
+        public double apply(double x, double y) {
+            return Math.pow(x, y);
+        }
+    },
+    REMAINDER("%") {
+        @Override
+        public double apply(double x, double y) {
+            return x % y;
+        }
+    };
+
+    private final String symbol;
+
+    ExtendedOperation(String symbol) {
+        this.symbol = symbol;
+    }
+
+
+    @Override
+    public String toString() {
+        return symbol;
+    }
+
+    public static void main(String[] args) {
+        double x = Double.parseDouble(args[0]);
+        double y = Double.parseDouble(args[1]);
+        test(ExtendedOperation.class, x, y);
+
+        test2(Arrays.asList(ExtendedOperation.values()), x, y);
+
+
+    }
+
+    // class 리터럴을 넘겨 확장된 연산들이 무엇인지 알려준다.
+    // <T extends Enum<T> & Operation>: Class 객체가 열거타입인 동시에 Operation의 하위 타입이 되어야 한다.
+    private static <T extends Enum<T> & Operation> void test(Class<T> opEnumType, double x, double y) {
+        for (Operation op : opEnumType.getEnumConstants()) {
+            System.out.printf("%f %s %f = %f%n", x, op, y, op.apply(x, y));
+        }
+    }
+
+    private static void test2(Collection<? extends Operation> opSet, double x, double y) {
+        for (Operation op : opSet) {
+            System.out.printf("%f %s %f = %f%n", x, op, y, op.apply(x, y));
+        }
+    }
+}
---
+++ src/main/java/item21_40/item38/Operation.java
@@ -0,0 +1,5 @@
+package item21_40.item38;
+
+public interface Operation {
+    double apply(double x, double y);
+}
